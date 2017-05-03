29695ad6a0359199781814f3273248ab047a07b6
Omnomon <jessselu@gmail.com>
2017-05-02 18:40:34 -0700

    save 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,58 +3,71 @@
 $(document).ready(function(){
 	// choose your fighter
 		// create fighter and enemy objects 
-	function fighter(hp, attack, defense, speed) {
+
+	function fighter(hp, attack, defense, speed, source) {
 		this.hp = hp;
 		this.attack = attack;
 		this.defense = defense;
 		this.speed = speed;
+		this.source = source;
 	}
 
-	function enemy(hp, counterattack, defense, speed) {
+	function enemy(hp, counterattack, defense, speed, source) {
 		this.hp = hp;
 		this.counterattack = counterattack;
 		this.defense = defense;
 		this.speed = speed;
+		this.source = source;
 	}
+
+	function getStats() {
+
+	}
+
 	var player; 
 	var computer;
-	var luke = new fighter(70, 90, 50, 60);
-	var yoda = new fighter(60, 40, 40, 90);
-	var han = new fighter(80, 50, 90, 50);
-	var vader = new enemy(90, 100, 90, 40);
-	var stormtrooper = new enemy(20, 20, 10, 30);
-	var atat = new enemy(100, 100, 90, 10);
 
-	//create secondary button for play 
+
+		var luke = new fighter(70, 90, 50, 60, "assets/images/lukeskywalker.jpg");
+		var yoda = new fighter(60, 40, 40, 90, "assets/images/yoda.jpg");
+		var han = new fighter(80, 50, 90, 50, "assets/images/hansolo.jpg");
+		var vader = new enemy(90, 100, 90, 40, "assets/images/darthvader.jpg");
+		var stormtrooper = new enemy(20, 20, 10, 30, "assets/images/stormtrooper.jpg");
+		var atat = new enemy(100, 100, 90, 10,"assets/images/atat.jpg");
+		var tiefighter = new enemy(20, 50, 40, 100,"assets/images/tiefighter.jpg");
+
+
+
+	var playerIndex = [luke, yoda, han];
+	var enemyIndex = [vader, stormtrooper, atat, tiefighter]	
+
+	// switch between pickChar and reset buttons 
 
 	$("#pickChar").click(function(){
 		console.log("pickChar button works")
 
-		if ($(".starter"))
-
-		// switch between pickChar and reset buttons 
 
 		$("#pickChar").addClass("hidden")
 		$(".character").addClass("hidden")
-		$("#userActions").removeClass("hidden")
 		$("#reset").removeClass("hidden")
-
-
-
-		//----------
+		$("#pickEnemy").removeClass("hidden")
+		$("#selector").removeClass("hidden")
 	})
+/*
+	$("#pickEnemy").click(function(){
+		$("#pickEnemy").removeClass("hidden")	
+	})*/
 
 	$("#reset").click(function(){
 		console.log("reset button works")
-
-		// switch between pickChar and reset buttons and action groups
 
 		$("#reset").addClass("hidden")
 		$(".character").removeClass("hidden")
 		$("#pickChar").removeClass("hidden")
 		$("#userActions").addClass("hidden")
-
-		//----------
+		$("#player_character").html("").attr("src","").addClass("hidden")
+		$("#pickEnemy").addClass("hidden")
+		$("#selector").addClass("hidden")
 
 		//----reset all vars 
 		var player = "";
@@ -62,27 +75,37 @@ $(document).ready(function(){
 
 	})
 
+	//----------------------------------- is there abetter way to do this =? 
 
-		// pick character to play as 
+	$("#lukeskywalker").data("value", luke)
+	$("#yoda").data("value", yoda)
+	$("#hansolo").data("value", han)
+	$("#vader").data("value", vader)
+	$("#stormtrooper").data("value", stormtrooper)
+	$("#atat").data("value", atat)
+	$("#tiefighter").data("value", tiefighter)
+
+
+		// pick character to play as, pick enemy to  ----- clean up later 
 
 	$("#lukeskywalker").click(function(){	
 /*		console.log("luke button works")*/
 		$(".btn-outline").removeClass("btn-primary")
-		$("#lukeskywalker").addClass("btn-primary")
-		player = luke
-		console.log(luke)
-
+		$(this).addClass("btn-primary")
+		console.log($(this)) 		
+		console.log($(this).data("value"))
 	}).on("mouseover", function(){
-		$(this).next(".popup").css('display', 'inline').html("<ul>HP:" + luke.hp +  "<ul>Attack:" + luke.attack + "<ul>Defense:" + luke.defense + "<ul>Speed:" + luke.speed);;
+		$(this).next(".popup").css('display', 'inline').html("<ul>HP:" + $(this).data("value").hp +  "<ul>Attack:" + $(this).data("value").attack + "<ul>Defense:" + $(this).data("value").defense + "<ul>Speed:" + $(this).data("value").speed);
 	}).on("mouseout", function(){
 		$(this).next('.popup').css('display', 'none');
 	})
 
 	$("#yoda").click(function(){
+		player = yoda
+
 /*		console.log("yoda button works")*/
 		$(".btn-outline").removeClass("btn-primary")
-		$("#yoda").addClass("btn-primary")
-		player = yoda
+		$(this).addClass("btn-primary")
 		console.log(yoda)
 
 
@@ -94,10 +117,11 @@ $(document).ready(function(){
 
 
 	$("#hansolo").click(function(){
+		player = han
+
 /*		console.log("han button works")*/
 		$(".btn-outline").removeClass("btn-primary")
-		$("#hansolo").addClass("btn-primary")
-		player = han 
+		$(this).addClass("btn-primary")
 		console.log(han)
 
 	}).on("mouseover", function(){
@@ -106,16 +130,34 @@ $(document).ready(function(){
 		$(this).next('.popup').css('display', 'none');
 	})
 
+	$("#pickChar").click(function(){
+		//player_character image is the image with btn-primary src 
+
+		$("#player_character").html($("<img>").attr("src",$(".btn-primary").attr("src") )).removeClass().addClass("img-thumbnail");
+
+		// check if playercharacter has object value 
+
+		console.log(player)
+	})
+
 	//-----------------------------
 
 
+// create pickEnemy 
+/*	$("#selector").click(function(){
+		computer =  
+	})
+*/
 
 
 
-// create handlers for buttons 
+// create handlers for buttons. 
+// each handle will call from object properties and combine differences 
+// it will also generate a predetermined stat boost, with a random value 
 
 	$("#attack").click(function(){
 		console.log("attack button works")
+
 	})
 
 	$("#defend").click(function(){
