126a98133894802fad033be36925e90c07563b26
Omnomon <jessselu@gmail.com>
2017-05-04 20:11:58 -0700

    save8

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
 
     // --------------------------set object values 
 
+
     var luke = new fighter(70, 90, 50, 60, "assets/images/lukeskywalker.jpg");
     var yoda = new fighter(60, 40, 40, 90, "assets/images/yoda.jpg");
     var han = new fighter(80, 50, 90, 50, "assets/images/hansolo.jpg");
@@ -34,6 +35,39 @@ $(document).ready(function() {
     var stormtrooper = new enemy(20, 20, 10, 30, "assets/images/stormtrooper.jpg");
     var atat = new enemy(100, 100, 90, 10, "assets/images/atat.jpg");
     var tiefighter = new enemy(20, 50, 40, 100, "assets/images/tiefighter.jpg");
+
+    //------------------------set object reset values 
+    function resetAllValues (){
+
+    luke = new fighter(70, 90, 50, 60, "assets/images/lukeskywalker.jpg");
+    yoda = new fighter(60, 40, 40, 90, "assets/images/yoda.jpg");
+    han = new fighter(80, 50, 90, 50, "assets/images/hansolo.jpg");
+    vader = new enemy(90, 100, 90, 40, "assets/images/darthvader.jpg");
+    stormtrooper = new enemy(20, 20, 10, 30, "assets/images/stormtrooper.jpg");
+    atat = new enemy(100, 100, 90, 10, "assets/images/atat.jpg");
+    tiefighter = new enemy(20, 50, 40, 100, "assets/images/tiefighter.jpg");
+
+        console.log("reset button works")
+
+        $("#reset").addClass("hidden")
+        $(".character").removeClass("hidden btn-primary")
+        $("#pickChar").removeClass("hidden")
+        $("#userActions").addClass("hidden")
+        $("#player_character").html("").attr("src", "").addClass("hidden").data("value", "")
+        $("#pickEnemy").addClass("hidden")
+        $("#selector").addClass("hidden")
+        $("#enemySelector").addClass("hidden")
+        $("#enemy_character").addClass("hidden")
+        $("#lukeskywalker").addClass("btn-primary")
+        $(".enemy").addClass("hidden")
+        $(".container").removeClass("hidden")
+        $(".statChange").addClass("hidden")
+
+        //----reset all vars 
+        var player = "";
+        var computer = "";
+
+    }
 
     //----------------------------------- set elements with attribute value, object 
 
@@ -113,28 +147,7 @@ $(document).ready(function() {
         console.log(player)
     })
 
-    $("#reset").click(function() {
-        console.log("reset button works")
-
-        $("#reset").addClass("hidden")
-        $(".character").removeClass("hidden btn-primary")
-        $("#pickChar").removeClass("hidden")
-        $("#userActions").addClass("hidden")
-        $("#player_character").html("").attr("src", "").addClass("hidden").data("value", "")
-        $("#pickEnemy").addClass("hidden")
-        $("#selector").addClass("hidden")
-        $("#enemySelector").addClass("hidden")
-        $("#enemy_character").addClass("hidden")
-        $("#lukeskywalker").addClass("btn-primary")
-        $(".enemy").addClass("hidden")
-        $(".container").removeClass("hidden")
-        $(".statChange").addClass("hidden")
-
-        //----reset all vars 
-        var player = "";
-        var computer = "";
-
-    })
+    $("#reset").click(resetAllValues)
 
     // create player character object 
 
@@ -177,14 +190,13 @@ $(document).ready(function() {
     $("#attack").click(function() {
     	attackCount++
         var computerLose;
-        console.log("attackCount is" + attackCount)
+        console.log("attackCount is" + (attackCount*Math.floor(10*Math.random())))
         console.log("attack button works")
         console.log("player hp is" + player.hp)
         console.log("computer hp is" + computer.hp)
   		var calcPlayerDmg = computer.counterattack - player.defense;
-  		calcPlayerBuff = player.attack + (10*attackCount*Math.floor(Math.random()))
   		if (calcPlayerDmg > 0) {
-        	player.hp = player.hp - calcDmg
+        	player.hp = player.hp - calcPlayerDmg
   		} else {
   			console.log("damage zero")
   		}
@@ -198,19 +210,24 @@ $(document).ready(function() {
 
         console.log(" new player hp is" + player.hp)
         console.log("new computer hp is" + computer.hp)
+       	$(".statChange").removeClass("hidden").html("Your HP is now " + player.hp + "<ul>Your Attack is now " + player.attack).css("color", "red")
+
         if (computer.hp <= 0 ) {
         	console.log("computer died")
-        	computerLose +=1 
+        	computerLose ++
         	console.log(computerLose)
         	$("#enemy_character").addClass("hidden")
-        	player.attack = player.attack += attackCount*5
-       		$(".statChange").removeClass("hidden").html("Your HP is now " + player.hp + "<ul>Your Attack is now " + player.attack).css("color", "red")
+        	player.attack = player.attack + attackCount*5
+       		
 
         } 
         if (player.hp <= 0) {
         	console.log("player died")
         	$("#player_character").addClass("hidden")
         }
+
+  		$(".statChange").removeClass("hidden").html("Your HP is now " + player.hp +"<ul>Your Attack is now "+ player.attack + "<ul> Your Defense is now " + player.defense + "<ul>Your speed is now " + player.speed).css("color", "red");
+
 
 
     })
@@ -222,27 +239,37 @@ $(document).ready(function() {
         console.log("defend button works")
         console.log(player.defense)
   		var calcPlayerDmg = computer.counterattack - player.defense;
-  		if (calcPlayerDmg > 0 ) {
-  			player.hp = player.hp - (calcPlayerDmg/2)
-  			player.defense = player.defense += (10*defenseCount*Math.floor(Math.random()))
-  			console.log("player defense is now" + player.defense)
-  			console.log(player.defense)
-  		}
-  		$(".statChange").removeClass("hidden").html("Your HP is now " + player.hp + "<ul> Your Defense is now " + player.defense).css("color", "red")
+  		player.hp = player.hp - 5;
+  		player.defense = player.defense + parseInt((defenseCount*Math.floor(10*Math.random())))
+  		
+  		/*var calcNewDefense = (10*defenseCount*Math.floor(Math.random()))*/
+  		console.log("player defense is now" + player.defense)
+
+  		$(".statChange").removeClass("hidden").html("Your HP is now " + player.hp +"<ul>Your Attack is now "+ player.attack + "<ul> Your Defense is now " + player.defense + "<ul>Your speed is now " + player.speed).css("color", "red");
         
     })
 
+    var dodgeCount = 0;
+
     $("#dodge").click(function() {
         console.log("dodge button works")
-		playerDodge = player.speed * Math.floor(Math.random())
-		computerDodge = computer.speed = Math.floor(Math.random())
+		playerDodge = player.speed * Math.floor(10*Math.random())
+		computerDodge = computer.speed * Math.floor(10*Math.random())
+		console.log(playerDodge)
+		console.log(computerDodge)
 		if ( playerDodge >= computerDodge ) {
-			computer.attack = computer.counterattack - 10 
+			computer.counterattack = computer.counterattack - 10 
+			console.log(computer.counterattack)
 		} else {
 			player.hp - player.hp - computer.counterattack
 		}
+  		$(".statChange").removeClass("hidden").html("Your HP is now " + player.hp +"<ul>Your Attack is now "+ player.attack + "<ul> Your Defense is now " + player.defense + "<ul>Your speed is now " + player.speed).css("color", "red");
+
 
     })
 
 
 })
+
+
+// write handlers directing player back to selection screen and hide attack/defnes/dodge button
